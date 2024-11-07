41f7308832b377ac29aeea525a4ce60be14a5568
AG <anthony.goncalves@fms-ea.com>
2024-11-07 11:52:09 +0100

    Moitié des questions moyennes

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ class Question {
 	enonce = "";
 	points = 1;
 	defaultAnswers = [1, 0, 0, 0]; // 1 = bonne réponse, 0 = mauvaise réponse, -1 = réponse masquée
-	answersText = ["", "", "", ""]; 
+	answersText = ["", "", "", ""];
 
 	constructor(enonce, answers, answersText) {
 		this.id = idQuestion++;
@@ -81,14 +81,14 @@ class Jeu {
 		answers[2] = document.getElementById("CheckC").checked ? 1 : 0;
 		answers[3] = document.getElementById("CheckD").checked ? 1 : 0;
 
-		let numQuestionTmp = this.numQuestionActuelle - 1;		
+		let numQuestionTmp = this.numQuestionActuelle - 1;
 
 		if (this.questions[numQuestionTmp].defaultAnswers[3] <= -1)
 			answers[3] = this.questions[numQuestionTmp].defaultAnswers[3];
-		
+
 		if (this.questions[numQuestionTmp].defaultAnswers[2] <= -1)
 			answers[2] = this.questions[numQuestionTmp].defaultAnswers[2];
-		
+
 		let time = this.tempsReponses - tempsActuel;
 
 		let pointsBonus = this.GetPointsFromTime(time, answers);
@@ -98,13 +98,12 @@ class Jeu {
 	}
 
 	ProchaineQuestion() {
-		if(this.numQuestionActuelle >= this.questions.length){
-			this.joueurs.sort( compare );
-			for(let i = 0; i < this.joueurs.length; i++)
+		if (this.numQuestionActuelle >= this.questions.length) {
+			this.joueurs.sort(compare);
+			for (let i = 0; i < this.joueurs.length; i++)
 				alert("#" + (this.joueurs.length - i) + " : " + this.joueurs[i].nom + " — " + this.joueurs[i].points);
-		} 
-		else
-		{
+		}
+		else {
 			this.joueurActuel = 1;
 			this.numQuestionActuelle++;
 		}
@@ -147,7 +146,7 @@ class Jeu {
 
 		document.getElementsByTagName("audio")[0].currentTime = 0;
 		document.getElementsByTagName("audio")[0].play();
-		
+
 		this.AfficherQuestion(this.numQuestionActuelle);
 	}
 
@@ -196,17 +195,17 @@ class Joueur {
 	}
 }
 
-function compare( a, b ) {
-	if ( a.points < b.points ){
-	  return -1;
+function compare(a, b) {
+	if (a.points < b.points) {
+		return -1;
 	}
-	if ( a.points > b.points ){
-	  return 1;
+	if (a.points > b.points) {
+		return 1;
 	}
 	return 0;
-  }
-  
- 
+}
+
+
 
 function NumberFormat(number, digits = 2) {
 	if ((number + "").length < digits) {
@@ -229,38 +228,76 @@ const questionsF = new Array();
 const questionsM = new Array();
 const questionH = new Array();
 
-questionsF.push(new Question("Je vois un panneau stop je m'arrête ?", [1, 0, -1, -1], ["Oui","Non","",""]));
-questionsF.push(new Question("Comment appelle-t-on un poisson qui sort de l'eau et traverse la route?", [1, 0, 0, -1], ["Un piéton","Un poisson volant","Un pied de thon",""]));
-questionsF.push(new Question("Pourquoi les feux de circulation ne jouent-ils jamais à cache-cache ?", [1, 0, -1, -1], ["Parce qu'ils ont peur de se faire griller !","Parce que les poulets traversent la route","",""]));
-questionsF.push(new Question("Si après avoir bu, vous conduisez en zigzagant, continuez-vous de conduire?", [1, 0, -1, -1], ["Non","Oui","",""]));
-questionsF.push(new Question("Je cède le passage :", [0, 1, -1, -1], ["A gauche","A droite","",""]));
-questionsF.push(new Question("Un bébé peut-il voyager dans les bras d'un adulte et jouer avec le volant?", [0, 1, -1, -1], ["Oui","Non","",""]));
-questionsF.push(new Question("Je roule à 90 km/h, pour m'arrêter il faudra :", [0, 1, 0, -1], ["une peau de banane","Une pédale de frein","faire une cascade",""]));
-questionsF.push(new Question("Je conduis depuis 3 heures, je suis fatigué :", [0, 1, 0, -1], ["J'accélère pour arriver plus vite","Je m'arrête pour faire une sieste","Je continue mon parcours",""]));
-questionsF.push(new Question("Je peux dépasser :", [1, 0, -1, -1], ["Oui","Non","",""]));
-questionsF.push(new Question("Je passe sous un tunnel :", [0, 1, 0, -1], ["J'appelle un ami","J'allume les phares","Je pile devant un camion",""]));
-questionsF.push(new Question("Quand il neige:", [1, 0, -1, -1], ["J'allume les phares","Je roule sans phares","",""]));
-questionsF.push(new Question("J'ai la possibilité de dépasser un cycliste:", [0, 1, 0, -1], ["Je lui balance une peau de banane","Je mets le clignotant et le dépasse","Je le pousse pour qu'il aille plus vite",""]));
-questionsF.push(new Question("Lors d'un contrôle de police, je dois présenter :", [0, 1, -1, -1], ["ma bouteille d'alcool","mon permis de conduire","",""]));
-questionsF.push(new Question("Ai-je le droit de stationner en plein milieu de la circulation?", [0, 1, -1, -1], ["Oui","Non","",""]));
-questionsF.push(new Question("Avec le permis B, je peux:", [1, 0, -1, -1], ["conduire une mario Kart","voler un vélo","",""]));
-questionsF.push(new Question("Le siège conducteur est réglable :", [0, 1, -1, -1], ["en douceur","en hauteur","",""]));
-questionsF.push(new Question("Il est conseiller de mettre ses essuies-glaces :", [0, 1, -1, -1], ["Quand il fait soleil","Quand il pleut","",""]));
-questionsF.push(new Question("Quand dois-je mettre la ceinture de sécurité:", [1, 0, 0, -1], ["Quand je suis en voiture","Quand je dors","Quand je me promène à pied",""]));
-questionsF.push(new Question("Après le panneau 50 km/h, je peux:", [1, 0, 0, -1], ["rouler à 50","arrêter la voiture","rouler à 110",""]));
-questionsF.push(new Question("Que devez-vous faire lorsque vous voyez le feu passer au rouge ?", [0, 1, -1, -1], ["Accélérer","S'arrêter","",""]));
-questionsF.push(new Question("Quel est le meilleur moment pour utiliser vos clignotants ?", [1, 0, -1, -1], ["Avant de tourner","Après avoir tourné","",""]));
-questionsF.push(new Question("Si vous entendez une sirène de police, que devez-vous faire ?", [0, 1, -1, -1], ["Ignorer","Se ranger sur le côté","",""]));
-questionsF.push(new Question("Quand devez-vous céder le passage ?", [0, 1, -1, -1], ["Toujours","Seulement quand il y a d'autres voitures","",""]));
-questionsF.push(new Question("Quel est l'effet de la vitesse sur votre temps de réaction ?", [0, 1, -1, -1], ["Cela l'améliore","Cela le ralentit","",""]));
-questionsF.push(new Question("Que devez-vous faire si un piéton traverse la route ?", [0, 1, -1, -1], ["Klaxonner","S'arrêter pour le laisser passer","",""]));
-questionsF.push(new Question("Si vous êtes fatigué en conduisant, que devez-vous faire ?", [0, 1, -1, -1], ["Ouvrir la fenêtre","Trouver un endroit pour vous reposer","",""]));
-questionsF.push(new Question("Que signifie un panneau de limitation de vitesse ?", [0, 1, -1, -1], ["Une suggestion","Une obligation","",""]));
-questionsF.push(new Question("Que devez-vous faire si vous êtes impliqué dans un accident ?", [0, 1, -1, -1], ["Fuir","Échanger des informations avec l'autre conducteur","",""]));
-questionsF.push(new Question("Quel est le rôle des rétroviseurs ?", [0, 1, -1, -1], ["Décorer la voiture","Aider à voir derrière vous","",""]));
-questionsF.push(new Question("Que devez-vous faire avant de changer de voie ?", [1, 0, -1, -1], ["Regarder dans votre rétroviseur","Ne rien faire","",""]));
+questionsF.push(new Question("Je vois un panneau stop je m'arrête ?", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+questionsF.push(new Question("Comment appelle-t-on un poisson qui sort de l'eau et traverse la route?", [1, 0, 0, -1], ["Un piéton", "Un poisson volant", "Un pied de thon", ""]));
+questionsF.push(new Question("Pourquoi les feux de circulation ne jouent-ils jamais à cache-cache ?", [1, 0, -1, -1], ["Parce qu'ils ont peur de se faire griller !", "Parce que les poulets traversent la route", "", ""]));
+questionsF.push(new Question("Si après avoir bu, vous conduisez en zigzagant, continuez-vous de conduire?", [1, 0, -1, -1], ["Non", "Oui", "", ""]));
+questionsF.push(new Question("Je cède le passage :", [0, 1, -1, -1], ["A gauche", "A droite", "", ""]));
+questionsF.push(new Question("Un bébé peut-il voyager dans les bras d'un adulte et jouer avec le volant?", [0, 1, -1, -1], ["Oui", "Non", "", ""]));
+questionsF.push(new Question("Je roule à 90 km/h, pour m'arrêter il faudra :", [0, 1, 0, -1], ["une peau de banane", "Une pédale de frein", "faire une cascade", ""]));
+questionsF.push(new Question("Je conduis depuis 3 heures, je suis fatigué :", [0, 1, 0, -1], ["J'accélère pour arriver plus vite", "Je m'arrête pour faire une sieste", "Je continue mon parcours", ""]));
+questionsF.push(new Question("Je peux dépasser :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+questionsF.push(new Question("Je passe sous un tunnel :", [0, 1, 0, -1], ["J'appelle un ami", "J'allume les phares", "Je pile devant un camion", ""]));
+questionsF.push(new Question("Quand il neige:", [1, 0, -1, -1], ["J'allume les phares", "Je roule sans phares", "", ""]));
+questionsF.push(new Question("J'ai la possibilité de dépasser un cycliste:", [0, 1, 0, -1], ["Je lui balance une peau de banane", "Je mets le clignotant et le dépasse", "Je le pousse pour qu'il aille plus vite", ""]));
+questionsF.push(new Question("Lors d'un contrôle de police, je dois présenter :", [0, 1, -1, -1], ["ma bouteille d'alcool", "mon permis de conduire", "", ""]));
+questionsF.push(new Question("Ai-je le droit de stationner en plein milieu de la circulation?", [0, 1, -1, -1], ["Oui", "Non", "", ""]));
+questionsF.push(new Question("Avec le permis B, je peux:", [1, 0, -1, -1], ["conduire une mario Kart", "voler un vélo", "", ""]));
+questionsF.push(new Question("Le siège conducteur est réglable :", [0, 1, -1, -1], ["en douceur", "en hauteur", "", ""]));
+questionsF.push(new Question("Il est conseiller de mettre ses essuies-glaces :", [0, 1, -1, -1], ["Quand il fait soleil", "Quand il pleut", "", ""]));
+questionsF.push(new Question("Quand dois-je mettre la ceinture de sécurité:", [1, 0, 0, -1], ["Quand je suis en voiture", "Quand je dors", "Quand je me promène à pied", ""]));
+questionsF.push(new Question("Après le panneau 50 km/h, je peux:", [1, 0, 0, -1], ["rouler à 50", "arrêter la voiture", "rouler à 110", ""]));
+questionsF.push(new Question("Que devez-vous faire lorsque vous voyez le feu passer au rouge ?", [0, 1, -1, -1], ["Accélérer", "S'arrêter", "", ""]));
+questionsF.push(new Question("Quel est le meilleur moment pour utiliser vos clignotants ?", [1, 0, -1, -1], ["Avant de tourner", "Après avoir tourné", "", ""]));
+questionsF.push(new Question("Si vous entendez une sirène de police, que devez-vous faire ?", [0, 1, -1, -1], ["Ignorer", "Se ranger sur le côté", "", ""]));
+questionsF.push(new Question("Quand devez-vous céder le passage ?", [0, 1, -1, -1], ["Toujours", "Seulement quand il y a d'autres voitures", "", ""]));
+questionsF.push(new Question("Quel est l'effet de la vitesse sur votre temps de réaction ?", [0, 1, -1, -1], ["Cela l'améliore", "Cela le ralentit", "", ""]));
+questionsF.push(new Question("Que devez-vous faire si un piéton traverse la route ?", [0, 1, -1, -1], ["Klaxonner", "S'arrêter pour le laisser passer", "", ""]));
+questionsF.push(new Question("Si vous êtes fatigué en conduisant, que devez-vous faire ?", [0, 1, -1, -1], ["Ouvrir la fenêtre", "Trouver un endroit pour vous reposer", "", ""]));
+questionsF.push(new Question("Que signifie un panneau de limitation de vitesse ?", [0, 1, -1, -1], ["Une suggestion", "Une obligation", "", ""]));
+questionsF.push(new Question("Que devez-vous faire si vous êtes impliqué dans un accident ?", [0, 1, -1, -1], ["Fuir", "Échanger des informations avec l'autre conducteur", "", ""]));
+questionsF.push(new Question("Quel est le rôle des rétroviseurs ?", [0, 1, -1, -1], ["Décorer la voiture", "Aider à voir derrière vous", "", ""]));
+questionsF.push(new Question("Que devez-vous faire avant de changer de voie ?", [1, 0, -1, -1], ["Regarder dans votre rétroviseur", "Ne rien faire", "", ""]));
+
+idQuestion = 1;
+
+questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+questionsM.push(new Question("Ce panneau électronique me signale que :", [1, 0, 0, 1], ["Je me trouve en excès de vitesse", "Je vais recevoir une amende", "Je passe devant une école", "Je dois ralentir"]));
+questionsM.push(new Question("Quels sont les risques lorsque je roule dans une telle forêt ? :", [1, 1, 1, 0], ["Percuter un animal sauvage", "Me perdre dans mon itinéraire", "Un croisement diffcile", "Devoir allumer les feux de route"]));
+questionsM.push(new Question("La conduite sous l'emprise de médicaments de manière encadrée est moins risquée que la conduite sous alcool : ", [0, 1, -1, -1], ["Oui", "Non", "", ""]));
+questionsM.push(new Question("Dans un rond point, je prends la deuxième sortie :", [0, 0, 1, 0], ["Je mets mon clignotant droit", "Je me place forcément sur la file de gauche", "Je cède le passage aux automobilistes arrivant à gauche", "Je pourrai dépasser les automobilistes de la file de gauche"]));
+questionsM.push(new Question("Je dois nécessairement m'arrêter :", [0, 1, -1, -1], ["Oui", "Non", "", ""]));
+questionsM.push(new Question("J'utilise les clignotants :", [1, 1, 1, 1], ["À une intersection", "En sortant d'un rond point", "Pour m'arrêter sur le bas-côté", "Lorsque je change de file sur l'autoroute"]));
+questionsM.push(new Question("Ce véhicule stationné risque :", [1, 0, 1, 1], ["Une amende", "Un retrait de points", "Un ramassage par la fourrière", "De gêner d'autres automobilistes"]));
+questionsM.push(new Question("J'achète une voiture, ma plaque d'immatriculation :", [0, 0, 1, 0], ["Peut être jaune à l'arrière", "Doit systématiquement être renouvelée", "Peut être associée au département de mon choix", "Peut contenir n'importe quelle lettre de l'alphabet"]));
+questionsM.push(new Question("Cette plaque signifie :", [0, 0, 1, -1], ["Que le véhicule est fabriqué en France", "Que le propriétaire habite dans le 71", "Que le véhicule est répertorié au niveau national", ""]));
+questionsM.push(new Question("Je passe au feu orange. Je risque une perte de points :", [0, 1, -1, -1], ["Oui", "Non", "", ""]));
+questionsM.push(new Question("Pour éviter un accident sur un coup de fatigue, je peux :", [1, 0, -1, -1], ["Emprunter des itinéraires plus rapides", "M'arrêter régulièrement", "Respecter les limitations de vitesse", "Etre prudent (c'est vraiment ma voiture donc ça n'arrive pas qu'aux autres !)"]));
+questionsM.push(new Question("Un panneau carré indique une obligation : ", [0, 1, -1, -1], ["Oui", "Non", "", ""]));
+questionsM.push(new Question("Dans cette situation :", [1, 1, 1, -1], ["Je quitte l'agglomération de Pradelles", "Je suis sur un axe prioritaire", "Je suis sur la route nationale N°88", ""]));
+questionsM.push(new Question("Ce panneau m'indique que je dois rouler à 50 km/h ou moins :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+questionsM.push(new Question("Quel panneau me signale un stationnement à durée limitée ?", [0, 1, 0, -1], ["Le panneau de gauche", "Le panneau central", "Le panneau de droite", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+// questionsM.push(new Question("Je vais à Arzens. Je suivrai la départementale N°33 :", [1, 0, -1, -1], ["Oui", "Non", "", ""]));
+
+idQuestion = 1;
+
+// questionsD.push...
 
 questionsDb.push(questionsF);
+questionsDb.push(questionsM);
 
 function Start() {
 	jeu = new Jeu();
